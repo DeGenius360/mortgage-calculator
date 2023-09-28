077f5f41f988d866b9b19d6c5cd8eb977bf0beb9
DeGenius360 <degenius8@gmail.com>
2023-09-28 16:27:20 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Container } from '@mui/material';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Container } from "@mui/material";
 
 const Navbar = () => {
   return (
     <div>
-       <AppBar position="static">
-        <Container maxWidth= "xl" >
-        <Toolbar>
-          <Typography variant="h5" component="p" >
-            Bank of DG
-          </Typography>
-          
-        </Toolbar>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h5" component="p">
+              Bank of DG
+            </Typography>
+          </Toolbar>
         </Container>
       </AppBar>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
